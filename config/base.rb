d086bbd231cfa7e74c70c47ffa1a7c1d3a9266f9
Thorben Schröder <thorben.schroeder@gmail.com>
2009-10-08 09:16:15 +0200

    Changed the documentation a little bit.

--- config/base.rb
+++ config/base.rb
@@ -1,5 +1,5 @@
 # This is called before anything which uses the config is called so use it
-# as a point to safely customize the CONFIG hash.
+# as a point to safely customize the CONFIG struct.
 
 # Remove the active modules like this:
 # CONFIG.active_modules.delete('map') # Prevents loading of the map module
@@ -8,4 +8,4 @@
 # 1. Change the log level:
 # CONFIG.logger.level = :error # Changes the log level to :error. See logger.rb for available log levels.
 # 2. Change the logger itself:
-# CONFIG.logger.class = "#{SRC_PATH}/my_fance_logger.rb" # Changes the used class to log messages.
+# CONFIG.logger.class = "#{LIB_PATH}/my_fancy_logger.rb" # Changes the used class to log messages.
